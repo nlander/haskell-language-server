da0ac643713656c04d72a7324a20ca2c9793c050
Elodie Lander <lander89@gmail.com>
2023-08-11 18:17:54 -0500

    WIP add test for different async versions

--- ghcide/test/exe/Dependency.hs
+++ ghcide/test/exe/Dependency.hs
@@ -31,6 +31,7 @@ tests :: TestTree
 tests =
     testGroup "gotoDefinition for dependencies"
         [ dependencyTest
+        , multiComponentDependencyTest
         ]
     where
         dependencyTest :: TestTree
@@ -70,3 +71,47 @@ tests =
                             bool Applicative.empty (pure fp) $
                                 fpSuffix `isSuffixOf` fpDirs
                         other -> error $ "Failed to parse ghcide/reference/ready file: " <> show other
+
+multiComponentDependencyTest :: TestTree
+multiComponentDependencyTest = testSessionWithExtraFiles "multi-component-dependency" "gotoDefinition in different versions of async" $
+    \dir -> do
+        docOld <- openDoc (dir </> "dependency-old" </> "Dependency" <.> "hs") "haskell"
+        docNew <- openDoc (dir </> "dependency-new" </> "Dependency" <.> "hs") "haskell"
+        _hieFile1 <- fileDoneIndexing ["Control", "Concurrent", "Async.hie"]
+        _hieFile2 <- fileDoneIndexing ["Control", "Concurrent", "Async.hie"]
+        defsOld <- getDefinitions docOld (Position 5 20)
+        defsNew <- getDefinitions docNew (Position 5 20)
+            -- The location of AsyncCancelled in async 2.2.1
+        let expRangeOld = Range (Position 357 22) (Position 430 36)
+            -- The location of AsyncCancelled in async 2.2.4
+            expRangeNew = Range (Position 430 22) (Position 430 36)
+        case defsOld of
+            InL (Definition (InR [Location fp actualRange])) ->
+                liftIO $ do
+                    let locationDirectories :: [String]
+                        locationDirectories =
+                            maybe [] splitDirectories $
+                                uriToFilePath fp
+                    assertBool "AsyncCancelled found in a module that is not Control.Concurrent Async"
+                        $ ["Control", "Concurrent", "Async.hs"]
+                            `isSuffixOf` locationDirectories
+                    actualRange @?= expRangeOld
+            wrongLocation ->
+                liftIO $
+                    assertFailure $ "Wrong location for AsyncCancelled in async 2.2.1: "
+                        ++ show wrongLocation
+        case defsNew of
+            InL (Definition (InR [Location fp actualRange])) ->
+                liftIO $ do
+                    let locationDirectories :: [String]
+                        locationDirectories =
+                            maybe [] splitDirectories $
+                                uriToFilePath fp
+                    assertBool "AsyncCancelled found in a module that is not Control.Concurrent Async"
+                        $ ["Control", "Concurrent", "Async.hs"]
+                            `isSuffixOf` locationDirectories
+                    actualRange @?= expRangeNew
+            wrongLocation ->
+                liftIO $
+                    assertFailure $ "Wrong location for AsyncCancelled in async 2.2.4: "
+                        ++ show wrongLocation
